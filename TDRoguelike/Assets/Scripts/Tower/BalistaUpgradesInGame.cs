5a44c81247c616d5502b9265dbc8102393b3c20c
Wyimaginowany <goralczyk.jakub@wp.pl>
2022-12-19 00:38:31 +0100

    balista animations

--- TDRoguelike/Assets/Scripts/Tower/BalistaUpgradesInGame.cs
+++ TDRoguelike/Assets/Scripts/Tower/BalistaUpgradesInGame.cs
@@ -7,10 +7,51 @@ using UnityEngine.UI;
 
 public class BalistaUpgradesInGame : TowerInGameUpgrades
 {
+    [SerializeField] private LeftUpgrades[] _leftUpgrades;
+    //[SerializeField] private GameObject[]
 
-
-    public override void UpgradeVisual(int upgradePurchasedIndex, int upgradeSide)
+    [System.Serializable]
+    private class RightUpgrades
     {
-        Debug.Log("index: " + upgradePurchasedIndex + " side: " + upgradeSide);
+        public GameObject[] TowerProjectile;
+    }
+
+    [System.Serializable]
+    private class LeftUpgrades
+    {
+        public GameObject UpgradeVisual;
+        public AnimatorOverrideController AnimatorOverride;
+    }
+
+    private Animator _animator;
+
+    protected override void Start()
+    {
+        base.Start();
+        _animator = GetComponent<Animator>();
+        _animator.runtimeAnimatorController = _leftUpgrades[0].AnimatorOverride;
+    }
+
+    public override void UpgradeVisual(int leftUpgradesPurchased, int rightUpgradesPurchased)
+    {
+        for (int i = 0; i < _leftUpgrades.Length; i++)
+        {
+            if (i == leftUpgradesPurchased)
+            {
+                _leftUpgrades[i].UpgradeVisual.SetActive(true);
+                _animator.runtimeAnimatorController = _leftUpgrades[i].AnimatorOverride;
+            }
+            else _leftUpgrades[i].UpgradeVisual.SetActive(false);
+        }
+    }
+
+    public void ShowProjectileModel()
+    {
+
+    }
+
+    public void HideProjectileModel()
+    {
+
     }
 }
