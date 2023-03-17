f2aac71e25da4fe4328dd4d72b5ca09e9d609bd9
Wyimaginowany <goralczyk.jakub@wp.pl>
2023-03-17 13:29:42 +0100

    pause hides base health

--- TDRoguelike/Assets/Scripts/Player/PlayerBase.cs
+++ TDRoguelike/Assets/Scripts/Player/PlayerBase.cs
@@ -13,6 +13,7 @@ public class PlayerBase : MonoBehaviour
     [Header("To Attach")]
     [SerializeField] private AudioClip _gameOverSound;
     [SerializeField] private TMP_Text _healthAmountText;
+    [SerializeField] private GameObject _playerBaseCanvas;
 
     private AudioSource _audioSource;
     private float _currentBaseHealth;
@@ -27,11 +28,26 @@ public class PlayerBase : MonoBehaviour
 
         _currentBaseHealth = _startBaseHealth;
         UpdatePlayerBaseHealthUI(_currentBaseHealth);
+
+        PauseManager.OnGamePaused += OnPaused;
+        PauseManager.OnGameResumed += OnResumed;
     }
 
     private void OnDestroy()
     {
         EnemyMovement.OnEnemyPathCompleate -= TakeDamage;
+        PauseManager.OnGamePaused -= OnPaused;
+        PauseManager.OnGameResumed -= OnResumed;
+    }
+
+    private void OnPaused()
+    {
+        _playerBaseCanvas.SetActive(false);
+    }
+
+    private void OnResumed()
+    {
+        _playerBaseCanvas.SetActive(true);
     }
 
     public void TakeDamage(float damage)
